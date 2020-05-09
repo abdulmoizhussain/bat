b0120f52245e35474840f1aca45c6f0daf8dee2e
Abdul Moiz <moizhus123@gmail.com>
2020-05-09 06:40:33 +0500

    disable ms word cursor animation

---
+++ disable ms word 2013 2016 cursor animation.bat
@@ -0,0 +1,3 @@
+REM https://www.laptopmag.com/articles/office-2013-typing-animation-disable
+REM https://superuser.com/questions/841122/how-can-i-add-a-new-dword-using-a-batch-script
+REG ADD HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\Common\Graphics /v DisableAnimations /t REG_DWORD /d 1

db39a8ed2bbe18009f20f474728b4e80faec3cf5
Abdul Moiz Hussain <33151673+abdulmoizhussain@users.noreply.github.com>
2020-09-12 17:19:02 +0500

    Create LaunchEmulatorFromCmd.bat

---
+++ LaunchEmulatorFromCmd.bat
@@ -0,0 +1,6 @@
+REM SOOURCE:
+REM https://developer.android.com/studio/run/emulator-commandline
+REM to list all the available emulators.
+REM %LocalAppData%\Android\Sdk\emulator\emulator -list-avds
+
+%LocalAppData%\Android\Sdk\emulator\emulator Nexus_5X_API_30

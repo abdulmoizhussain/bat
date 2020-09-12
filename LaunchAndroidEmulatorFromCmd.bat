d6b6279842feccbe58b500bb5f411d8d50d06d11
Abdul Moiz Hussain <33151673+abdulmoizhussain@users.noreply.github.com>
2020-09-12 17:20:55 +0500

    Update LaunchAndroidEmulatorFromCmd.bat

--- LaunchAndroidEmulatorFromCmd.bat
+++ LaunchAndroidEmulatorFromCmd.bat
@@ -3,4 +3,4 @@ REM https://developer.android.com/studio/run/emulator-commandline
 REM to list all the available emulators.
 REM %LocalAppData%\Android\Sdk\emulator\emulator -list-avds
 
-%LocalAppData%\Android\Sdk\emulator\emulator Nexus_5X_API_30
+%LocalAppData%\Android\Sdk\emulator\emulator -avd Nexus_5X_API_30

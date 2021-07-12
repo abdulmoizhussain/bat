ae9b29399b099d3a87d1473e1017200dcdcfc859
Abdul Moiz Hussain <33151673+abdulmoizhussain@users.noreply.github.com>
2021-07-12 22:16:56 +0500

    support for dual or more than 2 batteries laptops.

--- LaptopBatteryNotifier.sh
+++ LaptopBatteryNotifier.sh
@@ -1,76 +1,74 @@
 #!/usr/bin/env bash
 
 # Check shell dependencies by following commands, if they exist, then script will work fine otherwise install them:
-# acpi -h
+# upower -h
+# bc -h
 # zenity -h
 # notify-send --help
 # grep --help
 
-# change this accordingly only if you have dual battery laptop, otherwise leave it with 0.
-battery_to_watch=0
-notify_only_when_all_the_batteries_are_full_or_low=false
-sleep_time_in_seconds=240 # 4 minutes
-full_battery_percent=90
-low_battery__percent=25
+# Helpful Sources:
+# https://tldp.org/LDP/abs/html/comparison-ops.html
+# https://stackoverflow.com/a/16854326
+# https://stackoverflow.com/a/34286545
+# How to convert floating point number to integer:
+# https://unix.stackexchange.com/a/89748
+# Source, Bash comparison operators:
+# https://tldp.org/LDP/abs/html/comparison-ops.html
+
+full_battery_percent=95
+low_battery__percent=20
+
+sleep_time_in_seconds=300 # 5 minutes
+
 title_ac="On AC Power"
 title_dc="On Battery Power"
+energy_full_total='0'
+
+while read battery_path; do
+
+  temp=$(upower -i $battery_path | grep 'energy-full:' | grep -Eo '[0-9]+\.[0-9]+')
+  energy_full_total=$(bc -l <<<"(${temp}*100)+${energy_full_total}")
+
+done <<<$(upower -e | grep BAT)
+# this is why we need to create the while loop like this:
+# bash for loop save value in outer scope variable.
 
 while true; do
-  if [ $(acpi -b | grep -c Discharging) -eq 1 ]; then
-    # When laptop is NOT charging.
+  energy_now_total='0'
 
-    # only used when a specific battery is selected and notify_only_when_all_the_batteries_are_full_or_low is false.
-    battery_percent=$(acpi -b | grep "Battery $battery_to_watch" | grep -P -o '[0-9]+(?=%)')
+  while read battery_path; do
 
-    if $notify_only_when_all_the_batteries_are_full_or_low; then
-      are_batteries_low=true
-      for i in $(acpi -b | grep -P -o '[0-9]+(?=%)'); do
-        if [ $i -gt $low_battery__percent ]; then
-          are_batteries_low=false
-          break
-        fi
-      done
-      if $are_batteries_low; then
-        msg="~$low_battery__percent% power remaining.\nPlease Charge."
+    temp=$(upower -i $battery_path | grep 'energy:' | grep -Eo '[0-9]+\.[0-9]+')
+    energy_now_total=$(bc -l <<<"(${temp}*100)+${energy_now_total}")
 
-        notify-send "$title_dc" "$msg"
-        zenity --warning --title "$title_dc" --text "$msg" --width 200 &
-      fi
-      # if are_batteries_low
-    elif [ $battery_percent -lt $low_battery__percent ]; then
-      msg="$battery_percent% power remaining.\nPlease Charge."
+  done <<<$(upower -e | grep BAT)
 
+  energy_now_total=$(bc -l <<<"${energy_now_total}/${energy_full_total}*100")
+  # source,
+  energy_now_total=${energy_now_total%.*}
+
+  if [ $(upower -i $(upower -e | grep BAT) | grep 'state:' | grep -c discharging) -eq 1 ]; then
+    # when not charging
+
+    if [ $energy_now_total -le $low_battery__percent ]; then
+      msg="$energy_now_total% power remaining.\nPlease Charge."
       notify-send "$title_dc" "$msg"
       zenity --warning --title "$title_dc" --text "$msg" --width 200 &
     fi
+
   else
-    # When laptop is charging.
+    # when charging
 
-    # only used when a specific battery is selected and notify_only_when_all_the_batteries_are_full_or_low is false.
-    battery_percent=$(acpi -b | grep "Battery $battery_to_watch" | grep -P -o '[0-9]+(?=%)')
-
-    if $notify_only_when_all_the_batteries_are_full_or_low; then
-      are_batteries_full=true
-      for i in $(acpi -b | grep -P -o '[0-9]+(?=%)'); do
-        if [ $i -lt $full_battery_percent ]; then
-          are_batteries_full=false
-          break
-        fi
-      done
-      if $are_batteries_full; then
-        msg="Laptop ~$full_battery_percent% charged.\nRemove Charger."
-
-        notify-send "$title_ac" "$msg"
-        zenity --warning --title "$title_ac" --text "$msg" --width 200 &
-      fi
-    elif [ $battery_percent -gt $full_battery_percent ]; then
-      msg="Laptop $battery_percent% charged.\nRemove Charger."
-
+    if [ $energy_now_total -ge $full_battery_percent ]; then
+      msg="Laptop $energy_now_total% charged.\nRemove Charger."
       notify-send "$title_ac" "$msg"
       zenity --warning --title "$title_ac" --text "$msg" --width 200 &
     fi
+
   fi
 
-  # sleep 4m # Sleep for m minutes
   sleep $sleep_time_in_seconds
+  # OR
+  # sleep 5m # Sleep for 5 minutes
 done

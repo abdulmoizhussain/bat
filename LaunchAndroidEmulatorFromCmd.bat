b5772a57f1a25346717f6b57f15fa6ff2be3f1b9
Abdul Moiz Hussain <33151673+abdulmoizhussain@users.noreply.github.com>
2021-12-27 08:59:24 -0800

    added emulator.exe flags like: -netdelay -netspeed -gpu

--- LaunchAndroidEmulatorFromCmd.bat
+++ LaunchAndroidEmulatorFromCmd.bat
@@ -4,3 +4,9 @@ REM to list all the available emulators.
 REM %LocalAppData%\Android\Sdk\emulator\emulator -list-avds
 
 %LocalAppData%\Android\Sdk\emulator\emulator -avd Nexus_5X_API_30
+OR
+cmd /k %USERPROFILE%\AppData\Local\Android\Sdk\emulator\emulator -avd Pixel_2_API_30 -netdelay none -netspeed full -gpu host
+
+@REM -gpu auto|host (hardware) | angle_indirect (software, Windows only, uses DirectX instead of OpenGL) | swiftshader_indirect (software) | guest (lowest performance)
+@REM https://developer.android.com/studio/run/emulator-acceleration#accel-graphics
+
